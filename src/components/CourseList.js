202a50be731f1d00547b5b7717b28d2915ddbfe5
test <test@test.com>
2019-07-06 03:10:53 -0400

    made changes in prop types

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -27,7 +27,14 @@ function CourseList(props) {
 }
 
 CourseList.propTypes = {
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired
+    })
+  ).isRequired
 };
 
 export default CourseList;
