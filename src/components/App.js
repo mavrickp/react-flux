0f95d0820e5f9dedf5f2d946f141115cded159e2
test <test@test.com>
2019-07-06 02:35:52 -0400

    chnage class to function

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from "./common/Header";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
+import CoursesPage from "./CoursesPage";
+
+function App() {
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/courses") return <CoursesPage />;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Admin Pannel</h1>
+      <p>React, Flux and React Router for ultra responsive apps.</p>
+    </div>
+  );
+}
+
+export default HomePage;
