6880672514e8f06587803f3eda342d18631329b1
test <test@test.com>
2019-07-08 04:49:08 -0400

    forms modification

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
 
 const ManageCoursePage = props => {
+  const [errors, setErrors] = useState({});
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  useEffect(() => {
+    const slug = props.match.params.slug;
+    if (slug) {
+      courseApi.getCourseBySlug(slug).then(_course => setCourse(_course));
+    }
+  }, [props.match.params.slug]);
+
+  function handleChange({ target }) {
+    setCourse({
+      ...course,
+      [target.name]: target.value
+    });
+  }
+
+  function formIsValid() {
+    const _errors = {};
+
+    if (!course.title) _errors.title = "Tile is required";
+    if (!course.authorId) _errors.authorId = "Author Id is required";
+    if (!course.category) _errors.category = "Category is required";
+
+    setErrors(_errors);
+
+    return Object.keys(_errors).length === 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!formIsValid()) return;
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses");
+      toast.success("Course Saved.");
+    });
+  }
+
   return (
     <>
       <h2>Mage Course Page</h2>
-      {props.match.params.slug}
+      <CourseForm
+        course={course}
+        errors={errors}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
